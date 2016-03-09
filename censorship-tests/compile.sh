8ea218ad323e57baaddab64586e7b3077bae7925
ieee8023 <joseph@josephpcohen.com>
2016-03-09 12:52:34 -0500

    fix

--- censorship-tests/compile.sh
+++ censorship-tests/compile.sh
@@ -1,2 +1,2 @@
 mkdir -p classes
-javac -target 1.7 -source 1.7 -cp `sh getclasspath.sh` -d classes `find src -type f -name "*.java"`
+javac -cp .  -d classes `find src -type f -name "*.java"`
--- censorship-tests/getclasspath.sh
+++
@@ -1,1 +0,0 @@
-echo `find lib | tr '\n' ':'`
